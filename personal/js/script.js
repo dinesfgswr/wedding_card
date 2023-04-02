88118f295acdcbd50c825bc18dd5f45dd1b0f4ac
Dinesh Kumar Singh <83110005+dinesfgswr@users.noreply.github.com>
2023-04-02 11:02:59 +0530

    Update script.js

--- personal/js/script.js
+++ personal/js/script.js
@@ -5,7 +5,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 7, 2022 05:00:00").getTime();
+var countDownDate = new Date("May 7, 2023 05:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
